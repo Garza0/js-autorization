fcd1f1989b94b2660a23b06ce1c9dc481f9b78c2
Garza0 <konovalovandrey7@gmail.com>
2020-01-07 22:34:40 +0200

    firebase

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
+import { Question } from './question'
 import './style.css'
-
+import { isValid } from './utils'
 
 const form = document.getElementById('form')
 const input = form.querySelector('#question-input')
@@ -7,9 +8,27 @@ const submitBtn = form.querySelector('#submit')
 
 
 form.addEventListener('submit', submitFormHandler)
+input.addEventListener('input', () => {
+  submitBtn.disabled = !isValid(input.value)
+})
 
 function submitFormHandler(event) {
   event.preventDefault()
 
-  console.log(input.value)
+  if (isValid(input.value)) {
+    const question = {
+      text: input.value.trim(),
+      date: new Date().toJSON()
+    }
+    submitBtn.disabled = true
+    //Async request
+    Question.create(question).then(() => {
+      input.value = ''
+      input.className = ''
+      submitBtn.disabled = false
+    })
+
+
+
+  }
 }
