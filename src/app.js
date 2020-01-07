9a06963d0a43e944ccd73a9b678a6408be127a55
Garza0 <konovalovandrey7@gmail.com>
2020-01-07 21:11:05 +0200

    html

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import './style.css'
+
+
+const form = document.getElementById('form')
+const input = form.querySelector('#question-input')
+const submitBtn = form.querySelector('#submit')
+
+
+form.addEventListener('submit', submitFormHandler)
+
+function submitFormHandler(event) {
+  event.preventDefault()
+
+  console.log(input.value)
+}
